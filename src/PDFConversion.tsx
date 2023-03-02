78d427d3f4a6f16907ba2494401204667e9d14e8
TheMisterPenguin <ange.despert@gmail.com>
2023-03-02 15:58:35 +0100

    Finitions PDF

--- src/PDFConversion.tsx
+++ src/PDFConversion.tsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import {useRef, useState, useEffect, useId} from "react";
 import {Header, Segment, Icon, Button} from "semantic-ui-react";
 import {Helmet} from "react-helmet";
 
@@ -7,46 +7,49 @@ type PDFContent = {
 	pages: Array<String>;
 };
 
-function PDF2Text(file: File) {
+async function PDF2Text(file: File) {
 	const document: PDFContent = {filename: "", pages: []};
 	document.filename = file.name;
-	const convert = async (): Promise<void> => {
-		// On récupère le contenu du fichier
-		const buffer = await file.arrayBuffer();
-		// On récupère le contenu du document PDF
-        // @ts-ignore
-		const pdf = await pdfjsLib.getDocument(buffer).promise;
-        console.log(pdf.numPages);
-		// On lit toutes les pages
-		for (let i = 1; i < pdf.numPages; i++) {
-            console.log(i);
-			let page = await pdf.getPage(i);
-			let contenu = await page.getTextContent();
-			document.pages[i] += contenu.items
-				.map((s: any) => {
-					return s.str;
-				})
-				.join(" ");
-		}
-		return;
-	};
-	convert().then(() => {
-		console.log(document);
-	});
+	// On récupère le contenu du fichier
+	const buffer = await file.arrayBuffer();
+	// On récupère le contenu du document PDF
+	// @ts-ignore
+	const pdf = await pdfjsLib.getDocument(buffer).promise;
+	console.log(pdf.numPages);
+	// On lit toutes les pages
+	for (let i = 1; i < pdf.numPages; i++) {
+		console.log(i);
+		let page = await pdf.getPage(i);
+		let contenu = await page.getTextContent();
+		document.pages[i] += contenu.items
+			.map((s: any) => {
+				return s.str;
+			})
+			.join(" ");
+	}
+	console.log(document);
+    return document;
 }
 
 const PDFConversion = () => {
 	const inputRef = useRef(null);
-	const [iconColor, setIconColor] = useState<"black" | "red">("black");
+    const [file, setFile] = useState<File | undefined>(undefined);
 	const [displayFileName, setDisplayFileName] = useState<String | undefined>("Dépot de document PDF");
-	const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const file = e.target?.files?.item(0);
-		if (file) {
-			setDisplayFileName(file?.name);
-			setIconColor("red");
-			PDF2Text(file);
-		}
-	};
+	const [iconColor, setIconColor] = useState<"black" | "red">("black");
+    const [processingFile, setFileProcessing] = useState<boolean>(false);
+
+
+    // Traitement du fichier
+    useEffect(() => {
+        if (file) {
+            setDisplayFileName(file.name);
+            setIconColor("red");
+            setFileProcessing(true);
+            PDF2Text(file).then(doc => {
+                setFileProcessing(false);
+            });
+        }
+    }, [file]);
 
 	return (
 		<>
@@ -54,14 +57,14 @@ const PDFConversion = () => {
 				<title>PDF - LexiLab</title>
 			</Helmet>
 			<Segment>
-				<Segment placeholder>
+				<Segment placeholder loading={processingFile}>
 					<Header icon>
 						<Icon name="file pdf outline" color={iconColor} />
 						{displayFileName}
 					</Header>
 					{/* @ts-ignore*/}
-					<Button onClick={() => inputRef.current?.click()} color="black">
-						<input ref={inputRef} type="file" style={{display: "none"}} accept="application/pdf" onChange={handleFileUpload} />
+					<Button disabled={processingFile} onClick={() => inputRef.current?.click()} color="black">
+						<input ref={inputRef} type="file" style={{display: "none"}} accept="application/pdf" onChange={e => {if(e.target.files != undefined) setFile(e.target.files[0])}} />
 						Sélection
 					</Button>
 				</Segment>
