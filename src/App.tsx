82e2be33976240ef95ecc62ca93f11ba670854b8
AbdullahNox <114667402+AbdullahNox@users.noreply.github.com>
2023-03-18 17:32:47 +0100

    Add files via upload
    adaptation de la page en fonction de l'écran

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { Header, Grid, Divider, Segment, Button, Container, Form, Input, Dropdown, Select, Icon, Image } from "semantic-ui-react";
-import TextConversion from "./TextConversion";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
-import PDFConversion from "./PDFConversion";
+import {useEffect, useState} from "react";
+import {Header, Grid, Divider, Segment, Button, Container, Form, Input, Dropdown, Select, Icon, Image} from "semantic-ui-react";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
 import logo from "./assets/logo_2.svg";
-import MovieConversion from "./MovieConversion";
+import TextConversion from "./TextConversion";
 import YoutubeConversion from "./YoutubeConversion";
-import { Helmet } from "react-helmet";
-import "./App.css";
-import Login from "./Login";
+import MovieConversion from "./MovieConversion";
+import PDFConversion from "./PDFConversion";
+import {Helmet} from "react-helmet";
+import './style.css';
+
 
 /*Type qui peut prendre uniquement les chaines de caractères "text" "youtube" "movie" et "pdf"*/
 type buttonCategory = "text" | "youtube" | "movie" | "pdf";
@@ -24,15 +23,6 @@ function getSelectedCategory(): buttonCategory | undefined {
 
 function App() {
 	const [category, setCategory] = useState<buttonCategory | undefined>();
-	const [isLoginOpen, setIsLoginOpen] = useState(false);
-
-  	const handleLoginClick = () => {
-    	setIsLoginOpen(true);
-  	};
-
-  	const handleLoginClose = () => {
-    	setIsLoginOpen(false);
-  	};
 
 	useEffect(() => {
 		setCategory(getSelectedCategory());
@@ -41,23 +31,14 @@ function App() {
 	const navigate = useNavigate();
 	return (
 		<>
-
 			<Helmet>
 				<title>LexiLab</title>
 			</Helmet>
 			<Grid>
 				<Grid.Row />
 				<Grid.Row />
+				<Grid.Row />
 				<Grid.Row>
-					<Grid.Column width={16}>
-  						<Button floated="right" style={{ backgroundColor: "#FFB155", color: "white" }} onClick={handleLoginClick}>
-    						Se connecter
-  						</Button><Login isOpen={isLoginOpen} onClose={handleLoginClose} />
-					</Grid.Column>
-				</Grid.Row>
-				<Grid.Row>
-				</Grid.Row>
-				<Grid.Row >
 					<Image centered src={logo} size="medium" wrapped />
 				</Grid.Row>
 				<Grid.Row>
@@ -72,7 +53,7 @@ function App() {
 				<Grid.Row>
 					<Grid.Column width={2} />
 					<Grid.Column width={12}>
-						<Button.Group widths={4}>
+						<Button.Group widths={4} className="category-buttons">
 							<Button
 								size="big"
 								active={category === "text"}
@@ -80,7 +61,7 @@ function App() {
 									setCategory("text");
 									navigate("/texte");
 								}}>
-								<Icon name="keyboard outline" /> Texte
+								<Icon name="keyboard outline" /> <span className="button-text">Texte</span>
 							</Button>
 							<Button
 								size="big"
@@ -89,7 +70,7 @@ function App() {
 									setCategory("youtube");
 									navigate("/youtube");
 								}}>
-								<Icon name="youtube" /> Lien YouTube
+								<Icon name="youtube" /> <span className="button-text">Lien YouTube</span>
 							</Button>
 							<Button
 								size="big"
@@ -98,7 +79,7 @@ function App() {
 									setCategory("movie");
 									navigate("/film-serie");
 								}}>
-								<Icon name="film" /> Film / Série
+								<Icon name="film" /> <span className="button-text">Film / Série</span>
 							</Button>
 							<Button
 								size="big"
@@ -107,16 +88,9 @@ function App() {
 									setCategory("pdf");
 									navigate("/pdf");
 								}}>
-								<Icon name="file pdf outline" /> Document PDF
+								<Icon name="file pdf outline" /> <span className="button-text">Document PDF</span>
 							</Button>
 						</Button.Group>
-
-					</Grid.Column>
-					<Grid.Column width={2} />
-				</Grid.Row>
-				<Grid.Row textAlign="center">
-					<Grid.Column width={1} />
-					<Grid.Column width={14} textAlign="center">
 						<Routes>
 							<Route path="texte" element={<TextConversion />} />
 							<Route path="youtube" element={<YoutubeConversion />} />
@@ -124,7 +98,7 @@ function App() {
 							<Route path="pdf" element={<PDFConversion />} />
 						</Routes>
 					</Grid.Column>
-					<Grid.Column width={1} />
+					<Grid.Column width={2} />
 				</Grid.Row>
 			</Grid>
 		</>
