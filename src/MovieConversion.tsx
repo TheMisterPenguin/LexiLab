401c1754b2f45d5d74110d53c0d7ab3255c9ef86
abdoullah <abdoullah@23.com>
2023-03-23 17:42:58 +0100

    fichier de recherche des films

--- src/MovieConversion.tsx
+++ src/MovieConversion.tsx
@@ -1,39 +1,38 @@
-import {Segment, Grid, Form, Input, Button, Card, Icon, Image, Container, Header,  Dimmer, Loader} from 'semantic-ui-react'
-import {Helmet} from "react-helmet";
-import {useEffect, useState} from "react"
+import { Segment, Grid, Form, Input, Button, Card, Icon, Image, Container, Header, Dimmer, Loader } from 'semantic-ui-react'
+import { Helmet } from "react-helmet";
+import { useEffect, useState } from "react"
 
 type Movie = {
     id: number;
     rating: number;
-    language: string;
     age: number;
     title: string;
     image: string;
-    idFile: string;
+    idFile: number;
 };
-  
+
 type MovieData = {
     inputMovie: string;
     movieResults: Movie[];
 };
 
-//Fonction qui vérifie si l'image n'a pas d'erreur
-async function imageValide(url: string): Promise<number> {
+/*function sousTitre(id: number) {
+    const [idFile, setIdFile] = useState<number>(0);
+
+    setIdFile(id);
+
+    console.log(idFile);
+    return <p>ID de fichier sélectionné : {idFile}</p>;
+}*/
+
+//Fonction qui vérifie si l'image est éxploitable
+async function imageValide(url: string){
     try {
-        const response = await fetch(url);
-  
-        if (response.status === 200) {
-            return 200;
-        }
-        else if(response.status === 404){
-            return -1;
-        }
-        else {
-            return -1;
-        }
+        const response = await fetch(url, { method: 'HEAD' });
+        return response.ok;
     }
     catch (error) {
-        return -2;
+        return false;
     }
 }
 
@@ -50,24 +49,21 @@ function MovieConversion() {
         setMovieData({ ...movieData, inputMovie: event.target.value });
         //onChange={(e) => {setMovieData({ ...movieData, inputMovie: e.target.value })}}
     };
-
+    
     const fetchMovieConversion = async () => {
         setIsLoading(true);
 
-        const api = "5fYLrNmowziPVwKwhYS7TjE8goD4E7IU";
-        const enTete= new Headers({
-            "Access-Control-Request-Headers": "X_PINGOTHER, Content-Type",
+        const enTete = new Headers({
+            "api-key": "5fYLrNmowziPVwKwhYS7TjE8goD4E7IU",
             "Content-Type": "text/plain, application/x-www-form-urlencoded, charset=UTF-8",
             "Accept": "application/json",
-            "vary": "Accept-Encoding",
-            "Accept-encoding": "gzip, deflate, br",   
+            "Accept-encoding": "gzip, deflate, br",
         });
-        enTete.append("api-key", api);
 
         var nomFilm = (movieData.inputMovie);
         nomFilm = nomFilm.replaceAll(" ", "+");
 
-        var requete = new Request("https://api.opensubtitles.com/api/v1/subtitles?query="+nomFilm, {
+        var requete = new Request("https://api.opensubtitles.com/api/v1/subtitles?query=" + nomFilm, {
             method: 'GET',
             headers: enTete,
         });
@@ -75,58 +71,54 @@ function MovieConversion() {
         var donnee = await reponse.json();
         console.log(donnee);
 
-        //Mettre le premier caractère en majuscule
-        var st = movieData.inputMovie;
-        nomFilm = movieData.inputMovie.charAt(0).toUpperCase() + st.slice(1);
-        var compteur = 0;
-        const results: Movie[] = []; // Créer un nouveau tableau pour stocker les résultats des films
-        for(const res of donnee.data){
-            if(res.attributes.related_links != null){
+        var tabTemp: Movie[] = []; //Création d'une table temporaire pour récupérer les films
 
-                for(const lien of res.attributes.related_links){
-                    var im = lien.img_url;
-  
-                
+        for (const res of donnee.data) {
+            if (res.attributes.related_links != null) {
 
-                    // Ajouter les résultats de chaque film au tableau
-                    const status = await imageValide(im);
+                var idFilm = res.id;
+                var note = res.attributes.ratings;
+                var annee = res.attributes.feature_details.year;
+                var titre = res.attributes.feature_details.title;
+                var lienImage = res.attributes.related_links[0].img_url;
+                var idFile = res.attributes.files[0].file_id;
 
-                    if(status == 200){
-                        var idFilm = res.id;
-
-                        var note = res.attributes.ratings;
-
-                        var langue = res.attributes.language.toUpperCase();
-
-                        var annee = res.attributes.feature_details.year;
-
-                        var titre = res.attributes.feature_details.title;
-                        
-                        var lienImage = im;
-                        
-                        //for(const lien of res.attributes.related_links){
-                            
-                        //}
-
-                        for(const file of res.attributes.files){
-                            var idFile = file.file_id;
-                        }
-
-                        results.push({
-                            id: idFilm,
-                            language: langue,
-                            rating: note,
-                            age: annee,
-                            title: titre,
-                            image: lienImage,
-                            idFile: idFile
-                        });
-                    }
-                }
-                
+                tabTemp.push({
+                    id: idFilm,
+                    rating: note,
+                    age: annee,
+                    title: titre,
+                    image: lienImage,
+                    idFile: idFile
+                });
             }
         }
 
+        //Supprime les objets qui ont les mêmes titres et garder l'objet qui a la plus haute note
+        const supprimeRedondance: { [title: string]: Movie } = tabTemp.reduce((nbValeur, valActuel) => {
+            if (valActuel.title in nbValeur) {
+                if (valActuel.rating > nbValeur[valActuel.title].rating) {
+                    nbValeur[valActuel.title] = valActuel;
+                }
+            }
+            else {
+                nbValeur[valActuel.title] = valActuel;
+            }
+            return nbValeur;
+        }, {} as { [title: string]: Movie });
+
+        //Vérifie si l'image est exploitable(différents de 404), et ajouter l'objet
+        const results: Movie[] = await Promise.all(
+            Object.values(supprimeRedondance).map(async movie => {
+                const status = await imageValide(movie.image);
+                if (status) {
+                    return movie;
+                }
+                return null;
+            })
+        ).then(movies => movies.filter(movie => movie !== null) as Movie[]);
+
+        //Récupére les résultats
         setMovieData({
             ...movieData,
             movieResults: results // Mettre à jour le tableau avec les résultats des films
@@ -134,6 +126,13 @@ function MovieConversion() {
 
         setIsLoading(false);
     }
+
+    const [idFile, setIdFile] = useState<number>(0);
+    
+    const handleMovie = (id: number) => {
+        setIdFile(id);
+        console.log(id);
+    };
 
     return (
         <>
@@ -152,31 +151,26 @@ function MovieConversion() {
                 <Segment basic textAlign="center">
                     {isLoading ? (
                         <Loader active inline="centered" />
-                        ) : (
-                        <Button primary onClick={() => {fetchMovieConversion();}}>Extraire</Button>
+                    ) : (
+                        <Button primary onClick={() => { fetchMovieConversion(); }}>Extraire</Button>
                     )}
                 </Segment>
-
             </Segment>
 
-            <Grid columns={4}>
+            <Grid columns={3}>
                 {movieData.movieResults.map((movie) => (
                     <Grid.Column key={movie.id}>
                         <Segment basic textAlign="center">
-                            <Card onClick={() => console.log(`Clicked ${movie.idFile}`)}>
-                                <Image src={movie.image} wrapped ui={false} />
+                            <Card value = {idFile} onClick={() => handleMovie(movie.idFile)}>
+                                <Image src={movie.image} />
                                 <Card.Content>
                                     <Card.Header>{movie.title}</Card.Header>
                                     <Card.Meta>
-                                    <span className="date">{movie.age}</span>
+                                        <span className="date">{movie.age}</span>
                                     </Card.Meta>
-                                    <Card.Description>{movie.language}</Card.Description>
                                 </Card.Content>
                                 <Card.Content extra>
-                                    <a>
-                                        <Icon name="edit" />
-                                        {movie.rating}
-                                    </a>
+                                    <Icon name="edit" />{movie.rating}
                                 </Card.Content>
                             </Card>
                         </Segment>
