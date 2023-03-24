bf805a1cf3766c003370428ae1eeb706a9955f83
abdoullah <abdoullah@23.com>
2023-03-24 14:35:07 +0100

    correction movie

--- src/MovieConversion.tsx
+++ src/MovieConversion.tsx
@@ -1,119 +1,85 @@
-import {Segment, Grid, Form, Input, Button, Card, Icon, Image, Container, Header,  Dimmer, Loader} from 'semantic-ui-react'
-import {Helmet} from "react-helmet";
-import {useEffect, useState} from "react"
+import { Segment, Grid, Form, Input, Button, Card, Icon, Image, Container, Header, Dimmer, Loader } from 'semantic-ui-react'
+import { Helmet } from "react-helmet";
+import { useState } from "react"
+import ExtractWordGrid from "./ExtractWordGrid";
+
 
 type Movie = {
     id: number;
-    rating: number;
-    language: string;
     age: number;
     title: string;
     image: string;
     idFile: string;
 };
-  
-type MovieData = {
-    inputMovie: string;
-    movieResults: Movie[];
-};
 
-//Fonction qui vérifie si l'image n'a pas d'erreur
-async function imageValide(url: string): Promise<number> {
-    try {
-        const response = await fetch(url);
-  
-        if (response.status === 200) {
-            console.log('Le lien d\'image est valide et affichable.');
-            return 200;
-        }
-        else {
-            console.log('Le lien d\'image est invalide ou n\'est pas affichable.');
-        }
-    }
-    catch (error) {
-        console.error('Erreur de validation du lien d\'image:', error);
-    }
-    return -1; //ou une autre valeur que vous souhaitez retourner si le code de statut n'est pas 200
+async function getSearchedMovies(movieTitle : string) {
+    const res = await fetch("http://localhost:3001/api/getSearchedMovies", {
+        headers: {"Content-Type": "text/plain",},
+        body: movieTitle,
+        method: "POST",
+    })
+    
+    if(res.status !== 200) // si la requête n'a pas abouti
+        return null;
+    else
+        return await res.json();
+}
+
+async function getDownload(id : string){
+    const res = await fetch("http://localhost:3001/api/movieDownload", {
+        headers: {"Content-Type": "text/plain",},
+        body: id,
+        method: "POST",
+    })
+
+    if(res.status !== 200) // si la requête n'a pas abouti
+        return null;
+    else
+        return await res.json();
 }
 
 function MovieConversion() {
-    const [isLoading, setIsLoading] = useState(false);
+    const [fetching, setFetching] = useState(false);
+    const [movieTitle, setMovieTitle] = useState<string>("");
+    let [movies, setMovies] = useState<Movie[]>([]);
+    const [idFile, setIdFile] = useState<string>("");
+    const [subtitle, setSubtitle] = useState<{mot: string, niveau: string, type: string, traduction: string}[]>([]);
 
-    const [movieData, setMovieData] = useState<MovieData>({
-        inputMovie: "",
-        movieResults: [], // Tableau vide pour stocker les résultats des films
-    });    
 
     //Récupérer le contenu de l'input
-    const InputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setMovieData({ ...movieData, inputMovie: event.target.value });
-    };
-
+    const InputChange = (e : any) => {
+        setMovieTitle(e.target.value );
+    }
+    
     const fetchMovieConversion = async () => {
-        setIsLoading(true);
+        setFetching(true);
+        movieTitle.replaceAll(" ", "+");
 
-        const api = "5fYLrNmowziPVwKwhYS7TjE8goD4E7IU";
-        const enTete= new Headers({
-            "Accept": "application/json",    
-        });
-        enTete.append("api-key", api);
-
-        var nomFilm = (movieData.inputMovie);
-        nomFilm = nomFilm.replaceAll(" ", "+");
-
-        var requete = new Request("https://api.opensubtitles.com/api/v1/subtitles?query="+nomFilm, {
-            method: 'GET',
-            headers: enTete,
-        });
-        var reponse = await fetch(requete);
-        var donnee = await reponse.json();
-
-        //Mettre le premier caractère en majuscule
-        var st = movieData.inputMovie;
-        nomFilm = movieData.inputMovie.charAt(0).toUpperCase() + st.slice(1);
-        var compteur = 0;
-        const results: Movie[] = []; // Créer un nouveau tableau pour stocker les résultats des films
-        for(const res of donnee.data){
-            if(res.attributes.related_links != null){
-
-                var idFilm = res.id;
-
-                var note = res.attributes.ratings;
-
-                var langue = res.attributes.language.toUpperCase();
-
-                var annee = res.attributes.feature_details.year;
-
-                var titre = res.attributes.feature_details.title;
-
-                    
-                for(const lien of res.attributes.related_links){
-                    var lienImage = lien.img_url;
-                }
-
-                for(const file of res.attributes.files){
-                    var idFile = file.file_id;
-                }
-
-                // Ajouter les résultats de chaque film au tableau
-                results.push({
-                    id: idFilm,
-                    language: langue,
-                    rating: note,
-                    age: annee,
-                    title: titre,
-                    image: lienImage,
-                    idFile: idFile
-                });
+        getSearchedMovies(movieTitle).then((res) => {
+            if(res === null)
+                console.log("Erreur lors de la récupération des films");
+            else {
+                setMovies(res);
             }
-        }
-
-        setMovieData({
-            ...movieData,
-            movieResults: results // Mettre à jour le tableau avec les résultats des films
+            setFetching(false);
         });
+    }
 
-        setIsLoading(false);
+    //Récupérer l'id de film après un clique
+    const handleMovie = (id: string) => {
+        setIdFile(id);
+        console.log(id);
+
+        getDownload(idFile).then((res) => {
+            if(res === null){
+                console.log("Erreur lors de la récupération des sous titres");
+            }
+            else{
+                setSubtitle(res);
+                console.log(res);
+            }
+
+        });
     }
 
     return (
@@ -126,47 +92,39 @@ function MovieConversion() {
                 <Form>
                     <Form.Field>
                         <label>Saisir votre film :</label>
-                        <Input value={movieData.inputMovie} onChange={InputChange} placeholder='Rechercher...' />
+                        <Input value={movieTitle} onChange={ InputChange } placeholder='Rechercher...' />
                     </Form.Field>
                 </Form>
 
                 <Segment basic textAlign="center">
-                    {isLoading ? (
+                    {fetching ? (
                         <Loader active inline="centered" />
-                        ) : (
-                        <Button primary onClick={() => {fetchMovieConversion();}}>Extraire</Button>
+                    ) : (
+                        <Button primary onClick={() => { fetchMovieConversion(); }}>Extraire</Button>
                     )}
                 </Segment>
-
             </Segment>
 
-            
-                <Grid columns={4}>
-                {movieData.movieResults.map((movie) => (
-                    <Grid.Column  key={movie.id}>
+            <Grid columns={3}>
+                {movies.map((movie) => (
+                    <Grid.Column key={movie.id}>
                         <Segment basic textAlign="center">
-                            <Card>
-                                <Image src={movie.image} wrapped ui={false} />
+                            <Card value = {idFile} onClick={() => handleMovie(movie.idFile)}>
+                                <Image src={movie.image} />
                                 <Card.Content>
                                     <Card.Header>{movie.title}</Card.Header>
                                     <Card.Meta>
-                                        <span className='date'>{movie.age}</span>
+                                        <span className="date">{movie.age}</span>
                                     </Card.Meta>
-                                    <Card.Description>
-                                        {movie.language}
-                                    </Card.Description>
-                                </Card.Content>
-                                <Card.Content extra>
-                                    <a>
-                                        <Icon name='edit' />
-                                        {movie.rating}
-                                    </a>
                                 </Card.Content>
                             </Card>
                         </Segment>
                     </Grid.Column>
-                    ))}
-                </Grid>
+                ))}
+            </Grid>
+            {subtitle.length === 0 ? <></> :
+                <ExtractWordGrid props={subtitle as any} />
+            }
         </>
     )
 }
